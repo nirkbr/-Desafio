bab6fc0ba9d5d5d036a2b67e6e10fca6bb1b2c99
nirkbr <139012728+nirkbr@users.noreply.github.com>
2024-01-09 19:36:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("btn-vermelho").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","Vermelho");
+})
+
+document.getElementById("btn-azul").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","Azul");
+})
+
+document.getElementById("btn-verde").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","Verde");
+})
+
